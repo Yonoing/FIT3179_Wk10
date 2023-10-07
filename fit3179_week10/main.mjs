f63d0207f87f5fd5e0fc1f04a93904b825c05e61
Jonathan Lee <110045801+Yonoing@users.noreply.github.com>
2023-10-08 10:20:01 +1100

    Delete fit3179_week10 directory

--- fit3179_week10/main.mjs
+++
@@ -1,8 +0,0 @@
-try {
-	const vg_1 = "bank_branches_per_capita.vg.json"; 
-	const chart1 = await vegaEmbed("#chart1", vg_1); 
-	const vg_2 = "bank_deposits_states.vg.json"; 
-	const chart2 = await vegaEmbed("#chart2", vg_2); 
-} catch (e) {
-	console.error(e); 
-}
